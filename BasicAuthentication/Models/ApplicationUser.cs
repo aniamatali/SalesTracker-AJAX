776e74fbe00603d097f3ce69432961c523151047
Guest User <Guest@epicodus-8.local>
2017-10-23 11:36:36 -0700

    initial

---
+++ BasicAuthentication/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace BasicAuthentication.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+      
+    }
+}
